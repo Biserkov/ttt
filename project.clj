d559985255675edb5554003c41ff6c9d7ed16acd
Jordan Biserkov <biserkov@gmail.com>
2018-11-24 19:35:59 +0200

    Initial files.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ttt "0.1.0-SNAPSHOT"
+  :description "Inspired by the 'Functional Design in Clojure' podcast"
+  :url "https://github.com/Biserkov/ttt"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0-beta8"]])

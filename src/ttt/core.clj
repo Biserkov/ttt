5bb134e60c1b368e5a393a4c7adecdff515362d2
Jordan Biserkov <biserkov@gmail.com>
2018-11-28 00:06:13 +0200

    Fix requires.

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -1,8 +1,7 @@
 (ns ttt.core
   (:require [ttt.game :refer [game-state play new-board]]
-            [ttt.terminal :refer [pretty-print player]]))
-
-
+            [ttt.terminal :refer [pretty-print player]]
+            [ttt.game-test :refer [moves-win1 moves-win2 moves-draw moves-play]]))
 
 (defn -main []
   (let [board (new-board)
